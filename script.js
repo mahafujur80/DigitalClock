e47221cb03667312ec1f06167976bc239e9bd39d
MAHAFUJUR RAHMAN MUNNA <mahafujurrahman4480@gmail.com>
2025-11-28 10:42:40 +0600

    calculator

--- script.js
+++ script.js
@@ -1,47 +1,17 @@
-const display = document.querySelector("#displayTime");
-
-function whatTimeIsIt() {
-    const timeIs = new Date();
-    const nowTime = timeIs.toLocaleTimeString();
-    display.textContent = ` ${nowTime}`;
-
-    // 7 দিনের নাম (রোববার থেকে শনিবার)
-    const daysEnglish = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-    ];
-
-    // 12 মাসের নাম (জানুয়ারি থেকে ডিসেম্বর)
-    const monthsEnglish = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
-
-    // day
-    const days = timeIs.getDate();
-    const currentTime =
-        daysEnglish[timeIs.getDay()] +
-        " , " +
-        monthsEnglish[timeIs.getMonth()] +
-        " " +
-        days;
-    let display2 = (document.querySelector("#displayDate").textContent =
-        currentTime);
+function deleteMe() {
+    document.getElementById("myResult").value = " "
+}
+function calculator(NewValue) {
+    let display = document.getElementById("myResult");
+    display.value += NewValue;
+}
+function Answer() {
+    const a = document.getElementById("myResult").value;
+    const b = eval(a);
+    document.getElementById("myResult").value = b;
 }
 
-setInterval(whatTimeIsIt, 1000);
+function singleReomve() {
+    let input = document.getElementById("myResult");
+    input.value = input.value.slice(0, -1);
+}
